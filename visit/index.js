1b78c1f74840b7adcff29c0aab17be972cba0ac3
nishantpratap <60250376+nishantpratap@users.noreply.github.com>
2020-03-01 22:51:01 +0530

    Add files via upload

---
+++ visit/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const redis = require('redis');
+
+
+const app = express();
+const client = redis.createClient({
+   host: 'redis-server',
+   port: 6379      
+});
+client.set('visits', 0);
+
+
+
+app.get('/',(req, res) => {
+   client.get('visits',(err,visits) => {
+    res.send('Number of visits is '+ visits);
+    client.set('visits', parseInt(visits) + 1);
+   });
+});
+
+
+
+
+app.listen(9099, () => {
+   console.log('Listening on port 9099');
+});
